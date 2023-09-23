f35689972d77453a4b5ab3fd0c7d9c94b656a6ee
nipul07 <136158279+nipul07@users.noreply.github.com>
2023-09-24 00:04:21 +0200

    Add files via upload

---
+++ github.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "eu-west-2"
+  
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = "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"
+}
+
+resource "github_repository" "terra-ec2" {
+  name        = "terra-ec2"
+  description = "My awesome codebase"
+
+  visibility = "public"
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = "ami-0b1b00f4f0d09d131"
+    count = "1"
+    instance_type = "t2.micro"
+    key_name = "oscar"
+    subnet_id   = "subnet-03eb8c3e7ebe830aa"
+}
+
